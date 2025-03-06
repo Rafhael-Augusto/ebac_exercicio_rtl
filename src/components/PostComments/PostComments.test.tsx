5b7d7f4392ccd685cf0b03b09251c5c2cb09f2e5
Rafhael <rafhaelalvesprado@gmail.com>
2025-03-06 00:44:17 -0300

    testes jester

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,31 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import { fireEvent, render, screen } from "@testing-library/react";
+import PostComment from ".";
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
+describe("Teste para o componente PostComment", () => {
+  it("Deve renderizar o componente corretamente", () => {
+    render(<PostComment />);
+    expect(screen.getByText("Comentar")).toBeInTheDocument();
+  });
+
+  test("Deve adicionar um comentario nos comentarios", () => {
+    render(<PostComment />);
+    fireEvent.change(screen.getByTestId("campo-comment"), {
+      target: {
+        value: "Bem feio",
+      },
     });
+    fireEvent.click(screen.getByTestId("btn-comment"));
+    expect(screen.getByText("Bem feio")).toBeInTheDocument();
+  });
+
+  test("Deve adicionar outro comentario nos comentarios", () => {
+    render(<PostComment />);
+    fireEvent.change(screen.getByTestId("campo-comment"), {
+      target: {
+        value: "Bem legal",
+      },
+    });
+    fireEvent.click(screen.getByTestId("btn-comment"));
+    expect(screen.getByText("Bem legal")).toBeInTheDocument();
+  });
 });
